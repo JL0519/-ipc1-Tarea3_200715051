9e0590ac30ed4038f71dac5b97a727fdabe730bc
jl051 <jl051@192.168.1.12>
2018-08-19 15:42:30 -0600

    Iniciando tarea

---
+++ IPC1_Tarea3/src/Tarea/Tarea3_200715051.java
@@ -0,0 +1,12 @@
+package Tarea;
+
+public class Tarea3_200715051 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Iniciando Tarea 3");
+
+	}
+
+}
